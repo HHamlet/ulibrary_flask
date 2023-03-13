cff9ced74bf0e82eefe3cb1009d0918effc78c29
Hamlet Harutyunyan <hh.harutyunyan@mail.ru>
2023-03-14 00:30:07 +0400

    Problem 1 some improvment

--- Problem 1 ~ Find the Duplicates.py
+++ Problem 1 ~ Find the Duplicates.py
@@ -1,11 +1,19 @@
-def find_duplicates(num: list):
+from typing import Any
+
+
+def find_duplicates(nums: list) -> list[Any] | None:
+    temp = []
     duplicate = set()
-    for i in num:
-        if num.count(i) > 1:
+    for i in nums:
+        if i not in temp:
+            temp.append(i)
+        else:
             duplicate.add(i)
-    print(*duplicate, sep=",")
-    return
+    if len(duplicate) == 0:
+        return None
+    else:
+        return list(duplicate)
 
 
 num = [6, 2, 5, 2, 6, 2]
-find_duplicates(num)
+print(find_duplicates(num))
