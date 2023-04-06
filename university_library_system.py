ae73df62414e1f8608bd0f4bad62c1e2419dbcb6
Hamlet Harutyunyan <hh.harutyunyan@mail.ru>
2023-04-07 00:30:46 +0400

    1st changes

--- university_library_system.py
+++ university_library_system.py
@@ -12,8 +12,13 @@ class Book:
     def __str__(self):
         return f"{self.title}, Author: {self.author_name} {self.author_surname}"
 
+    # def __eq__(self, other):
+    #     return (self.title == other.title) and (self.author_name == other.author_name) and (self.author_surname == other.author_surname)
+
 
 class Library:
+    storage = []
+
     @classmethod
     def display(cls):
         with open("db.csv", "r") as f:
@@ -23,38 +28,56 @@ class Library:
 
     @classmethod
     def storage_init(cls):
-        storage = []
         with open("db.csv", "r") as f:
             csv_read = csv.DictReader(f)
             for row in csv_read:
-                storage.append(row)
-        return storage
+                cls.storage.append(row)
+        return cls.storage
 
-    def add_to_db(*args):
-        with open("db.csv", "a") as f:
+    @classmethod
+    def add_to_storage(cls, *args):
+        for item in args:
+            item_dict = {"Title": item.title,
+                         "Name": item.author_name,
+                         "Surname": item.author_surname,
+                         "ISBN": item.isbn,
+                         "Year": item.year}
+            cls.storage.append(item_dict)
+        return cls.storage
+
+    @staticmethod
+    def add_to_db(book_list):
+        with open("db.csv", "w") as f:
             fieldnames = ["Title", "Name", "Surname", "ISBN", "Year"]
             csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
-            csv_writer.writeheader()
-            for item in args:
-                item_dict = {"Title": item.title,
-                             "Name": item.author_name,
-                             "Surname": item.author_surname,
-                             "ISBN": item.isbn,
-                             "Year": item.year}
-                csv_writer.writerow(item_dict)
+            # csv_writer.writeheader()
+            for item in book_list:
+                csv_writer.writerow(item)
 
 
 class Students:
-    pass
+    def __init__(self, name, id, email):
+        self.name = name
+        self.id = id
+        self.email = email
+        self.book_current_taken = ()
+        self.book_limit = 5
 
 
 book1 = Book("book1", "name1", "surname1")
 book2 = Book("book2", "name2", "surname2")
 book3 = Book("book3", "name3", "surname3")
 print(book3)
-# Library.add_to_db(book1, book2, book3)
+Library.add_to_storage(book1, book2, book3)
+Library.add_to_db(Library.storage)
 book4 = Book("book4", "name4", "surname4")
-# Library.add_to_db(book4)
+book5 = Book("book5", "name5", "surname5")
+Library.add_to_storage(book4)
+Library.add_to_storage(book5)
+book6 = Book("book1", "name1", "surname1")
+Library.add_to_storage(book6)
 lib = Library()
-print(Library.storage_init())
-lib.display()
+# print(Library.storage_init())
+# lib.display()
+Library.add_to_db(Library.storage)
+print(book1.title == book6.title)
