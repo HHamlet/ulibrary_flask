abbf2d60dbc25570d8f571b15f2a63707426a96c
Hamlet Harutyunyan <hh.harutyunyan@mail.ru>
2023-04-18 23:41:07 +0400

    rev01

--- university_library_system.py
+++ university_library_system.py
@@ -1,95 +1,151 @@
 import csv
+import random
+
+
+class Author:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.book = set()
+
+    def __repr__(self):
+        return f"{self.name},{self.surname}"
+
+    def __hash__(self):
+        return hash((self.name, self.surname))
+
+    def __eq__(self, other):
+        return (self.name == other.name) and (self.surname == other.surname)
+
+    def new_book(self, title, year=None, isbn=None):
+        new_book = Book(title, year, isbn)
+        self.book.add(new_book)
+        return new_book
 
 
 class Book:
-    def __init__(self, title, author_name, author_surname, year=None):
+    def __init__(self, title, author, year=None, isbn=None):
         self.title = title
-        self.author_name = author_name
-        self.author_surname = author_surname
-        self.isbn = f"{hash(self.title)}"
-        self.year = None
+        self.author = author
+        self.year = year
+        self.isbn = isbn
+        self.copies = []
 
-    def __str__(self):
-        return f"{self.title}, Author: {self.author_name} {self.author_surname}"
+    def __repr__(self):
+        return f"{self.title} {self.author.name} {self.author.surname}, {self.year}, {self.isbn}"
 
-    # def __eq__(self, other):
-    #     return (self.title == other.title) and (self.author_name == other.author_name) and (self.author_surname == other.author_surname)
+    def add_copy(self):
+        book_copy = BookCopy(self)
+        self.copies.append(book_copy)
+        return self.copies
+
+    def __hash__(self):
+        return hash((self.title, self.author))
+
+    def __eq__(self, other):
+        return (self.title == other.title) and (self.author == other.author)
+
+
+class BookCopy:
+    def __init__(self, book):
+        self.book = book
+        self.year = book.year
+        self.isbn = book.isbn
+
+    def __repr__(self):
+        return f"copy: {self.book.title} {self.year},ISBN :{self.isbn}"
 
 
 class Library:
-    storage = []
+    storage = {}
+    students_list = {}
 
     @classmethod
-    def display(cls):
-        with open("db.csv", "r") as f:
-            csv_read = csv.DictReader(f)
-            for row in csv_read:
-                print(row["Title"], row["Name"], row["Surname"], row["ISBN"], row["Year"])
+    def add_to_storage(cls, title, name, surname, year=None, isbn=None):
+        author = Author(name, surname)
+
+        if author not in cls.storage:
+            cls.storage[author] = {}
+
+        book = Book(title, author, year, isbn)
+
+        if book not in cls.storage[author]:
+            cls.storage[author][book] = book.copies
+        else:
+            cls.storage[author][book] = book.add_copy()
 
     @classmethod
     def storage_init(cls):
         with open("db.csv", "r") as f:
-            csv_read = csv.DictReader(f)
+            fieldnames = ["name", "surname", "title", "copy"]
+            csv_read = csv.DictReader(f, fieldnames=fieldnames)
             for row in csv_read:
-                cls.storage.append(row)
-        return cls.storage
-
-    @classmethod
-    def add_to_storage(cls, *args):
-        for item in args:
-            item_dict = {"Title": item.title,
-                         "Name": item.author_name,
-                         "Surname": item.author_surname,
-                         "ISBN": item.isbn,
-                         "Year": item.year}
-            cls.storage.append(item_dict)
+                author = Author(row["name"], row["surname"])
+                cls.storage[author] = {}
+                book = Book(row["title"], author)
+                cls.storage[author][book] = row["copy"]
         return cls.storage
 
     @staticmethod
     def add_to_db(book_list):
         with open("db.csv", "w") as f:
-            fieldnames = ["Title", "Name", "Surname", "ISBN", "Year"]
+            fieldnames = ["name", "surname", "title", "copy"]
             csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
-            # csv_writer.writeheader()
-            for item in book_list:
-                csv_writer.writerow(item)
+            for key, value in book_list.items():
+                dict_1 = {"name": key.name,
+                          "surname": key.surname
+                          }
+                for el in value:
+                    dict_2 = {"title": el,
+                              "copy": value[el]
+                              }
+                    csv_writer.writerow({"name": dict_1["name"], "surname": dict_1["surname"],
+                                         "title": dict_2["title"], "copy": dict_2["copy"]})
 
     @classmethod
     def request(cls, title, name, surname):
         request_flag = False
         for items in cls.storage:
-            if (title == items["Title"]) and (name == items["Name"]) and (surname == items["Surname"]):
-                request_flag = True
+            if surname == items.surname and name == items.name:
+                for el in cls.storage[items]:
+                    if title == el.title:
+                        if len(el.copies) >= 1:
+                            request_flag = True
         return request_flag
 
-    def sign_book(self):
+    @classmethod
+    def students_registration(cls, name, email):
+        student = Students(name, email)
+        if student not in cls.students_list:
+            cls.students_list[student] = [student.id, student.book_current_taken]
+        else:
+            return f"{student} is registered"
+        return cls.students_list
+
+    @classmethod
+    def sign_book(cls):
         pass
 
 
 class Students:
-    def __init__(self, name, id, email):
+    def __init__(self, name, email):
         self.name = name
-        self.id = id
+        self.id = str(random.getrandbits(30))
         self.email = email
         self.book_current_taken = ()
         self.book_limit = 5
 
+    def __repr__(self):
+        return f"Student ({self.name}, {self.email})"
 
-book1 = Book("book1", "name1", "surname1")
-book2 = Book("book2", "name2", "surname2")
-book3 = Book("book3", "name3", "surname3")
-print(book3)
-Library.add_to_storage(book1, book2, book3)
-Library.add_to_db(Library.storage)
-book4 = Book("book4", "name4", "surname4")
-book5 = Book("book5", "name5", "surname5")
-Library.add_to_storage(book4)
-Library.add_to_storage(book5)
-book6 = Book("book1", "name1", "surname1")
-Library.add_to_storage(book6)
-lib = Library()
-# print(Library.storage_init())
-# lib.display()
-Library.add_to_db(Library.storage)
-print(book1.title == book6.title)
-print(Library.request("book2", "name2", "surname2"))
+
+Library.add_to_storage("War of the Worlds", "Herbert", "Wells", 1898, "789456")
+Library.add_to_storage("War of the Worlds", "Herbert", "Wells", 1898, "789456")
+Library.add_to_storage("War of the Worlds", "Herbert", "Wells", 1898, "789456")
+Library.add_to_storage("The White Man's Burden", "Rudyard", "Kipling", 1899, "7894887")
+Library.add_to_storage("The Jungle Book", "Rudyard", "Kipling", 1894, "7894127")
+Library.add_to_storage("The Jungle Book", "Rudyard", "Kipling", 1894, "7894127")
+# Library.add_to_db(Library.storage)
+# Library.storage_init()
+print(Library.storage)
+print(Library.request("War of the Worlds", "Herbert", "Wells"))
