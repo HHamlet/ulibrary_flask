e474bd73f353645b969ecd161d895a749de546dd
Hamlet Harutyunyan <hh.harutyunyan@mail.ru>
2023-05-05 00:17:21 +0400

    V02 rebuilt !

--- university_library_system_v02.py
+++ university_library_system_v02.py
@@ -1,5 +1,6 @@
 import json
 import random
+from datetime import datetime
 
 
 class BaseModel:
@@ -77,12 +78,15 @@ class Book(BaseModel):
         return (self.title == other.title) and (self.author == other.author)
 
 
-class BookCopy(Book):
+class BookCopy(BaseModel):
     def __init__(self, title, author_name, author_surname, year, isbn):
         self.title = title
         self.author = (author_name, author_surname)
         self.year = year
         self.isbn = isbn
+        self.status = "Available"
+        self.borrow_data = None
+        self.borrow_student = None
 
     def __repr__(self):
         return f"copy: {self.title} , {self.author}, {self.year}, {self.isbn}"
@@ -108,8 +112,13 @@ class Library(BaseModel):
             book = Book(items["title"], items["author"]["name"], items["author"]["surname"],
                         items["year"], items["isbn"])
             if len(items["copies"]) != 0:
-                for i in range(len(items["copies"])):
-                    book.add_copy(book)
+                for element in items["copies"]:
+                    book_copy = BookCopy(element["title"], element["author"][0], element["author"][1],
+                                         element["year"], element["isbn"])
+                    book_copy.status = element["status"]
+                    book_copy.borrow_data = element["borrow_data"]
+                    book_copy.borrow_student = element["borrow_student"]
+                    book.copies.append(book_copy)
             cls.book_object_list.append(book)
 
     @staticmethod
@@ -135,8 +144,11 @@ class Library(BaseModel):
         request_flag = False
         for items in cls.book_object_list:
             if title == items.title and (name == items.author.name and surname == items.author.surname):
-                request_flag = True
-                return items
+                for book_copy in items.copies:
+                    if book_copy.status == "Available":
+                        request_flag = True
+                        print(request_flag)
+                        return book_copy
         return request_flag
 
     @classmethod
@@ -150,14 +162,20 @@ class Library(BaseModel):
 
     @classmethod
     def sign_book(cls, title, author_name, author_surname, stud_name, stud_surname):
-        if isinstance(Library.request(title, author_name, author_surname), Book):
+        if isinstance(Library.request(title, author_name, author_surname), BookCopy):
             book_for_sign = Library.request(title, author_name, author_surname)
             for student in cls.students_object_list:
                 if stud_name == student.name and stud_surname == student.surname:
-                    if student.book_limit > 0:
-                        student.book_current_taken.add(book_for_sign.copies[0])
-                        book_for_sign.copies.remove(book_for_sign.copies[0])
-                        student.book_limit -= 1
+                    if len(student.book_current_taken) < student.book_limit:
+                        if len(student.book_current_taken) >= 1:
+                            for cb in student.book_current_taken:
+                                delta = datetime.now() - cb.borrow_data
+                                if delta.days > 14:
+                                    return f"{student} not returned book in 14 days!!!"
+                        student.book_current_taken.add(book_for_sign)
+                        book_for_sign.status = "Not available"
+                        book_for_sign.borrow_student = (student.name, student.surname, student.email)
+                        book_for_sign.borrow_data = str(datetime.today())
                         print(f"{book_for_sign} signed to {student}")
                         return cls.students_object_list
                     else:
@@ -176,8 +194,35 @@ class Library(BaseModel):
                 for element in items["book_current_taken"]:
                     book_copy = BookCopy(element["title"], element["author"][0],
                                          element["author"][1], element["year"], element["isbn"])
-                    student.book_current_taken.add(book_copy)
+                    book_copy.status = element["status"]
+                    book_copy.borrow_data = datetime.strptime(element["borrow_data"], "%Y-%m-%d %H:%M:%S.%f")
+                    book_copy.borrow_student = element["borrow_student"]
+                    for some_book in Library.book_object_list:
+                        if book_copy.title == some_book.title and book_copy.author[0] == some_book.author.name and \
+                                book_copy.author[1] == some_book.author.surname:
+                            for books in some_book.copies:
+                                if books.borrow_student == book_copy.borrow_student:
+                                    student.book_current_taken.add(books)
             Library.students_object_list.append(student)
+
+    @classmethod
+    def search_student(cls, student_name, student_surname):
+        for student in cls.students_object_list:
+            if student_name == student.name and student_surname == student.surname:
+                print("student find!")
+                return student
+
+    @classmethod
+    def book_return(cls, title, author_name, author_surname, student_name, student_surname):
+        student = cls.search_student(student_name, student_surname)
+        print("current taken ", student.book_current_taken)
+        for book_copy in student.book_current_taken:
+            if title == book_copy.title and author_name == book_copy.author[0] and author_surname == book_copy.author[1]:
+                book_copy.status = "Available"
+                book_copy.borrow_data = None
+                book_copy.borrow_student = None
+                student.book_current_taken.remove(book_copy)
+                break
 
 
 class Students(BaseModel):
@@ -198,38 +243,3 @@ class Students(BaseModel):
     def __eq__(self, other):
         return (self.name == other.name) and (self.surname == other.surname)
 
-
-# b1 = Book("War of the Worlds", "Herbert", "Wells", 1898, "9781595400307")
-# b2 = Book("War of the Worlds", "Herbert", "Wells", 1898, "9781595400307")
-# b3 = Book("War of the Worlds", "Herbert", "Wells", 1898, "9781595400307")
-# b4 = Book("War of the Worlds", "Herbert", "Wells", 1898, "9781595400307")
-# b5 = Book("The White Man's Burden", "Rudyard", "Kipling", 1899, "9780199210824")
-# b6 = Book("The White Man's Burden", "Rudyard", "Kipling", 1899, "9780199210824")
-# b7 = Book("Dune", "Frank", "Herbert", 1965, "9780441013593")
-# Library.add_to_storage(b1, b2, b3, b4, b5, b6, b7)
-# Library.write_to_db(JSONSerializer.to_json(Library.book_object_list), "new.json")
-Library.storage_init()
-
-b8 = Book("Dune", "Frank", "Herbert", 1965, "9780441013593")
-Library.add_to_storage(b8)
-print(Library.book_object_list)
-
-
-# Library.write_to_db(JSONSerializer.to_json(Library.book_object_list), "new.json")
-# print(Library.request("The White Man's Burden", "Rudyard", "Kipling"))
-# Library.students_registration("Adam", "Smith", "adam.smith@domain.com")
-# Library.students_registration("Eva", "Schneider", "eva.schneider@domain.com")
-# Library.students_registration("Tom", "Sawyer", "tom.sawyer@domain.com")
-# print(Library.students_object_list)
-# Library.sign_book("War of the Worlds", "Herbert", "Wells", "Eva", "Schneider")
-
-# for el in Library.students_object_list:
-#     print(el)
-#     print(el.book_current_taken, el.book_limit)
-# for el in Library.book_object_list:
-#     print(el.copies, len(el.copies))
-# Library.write_to_db(JSONSerializer.to_json(Library.students_object_list), "student.json")
-Library.student_list_init()
-for el in Library.students_object_list:
-    print(el)
-    print(el.book_current_taken, el.book_limit)
