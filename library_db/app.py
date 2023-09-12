4210c99ad2d4c268d5131a96acbdbfbcfe2c3c63
Hamlet <hh.harutyunyan@mail.ru>
2023-09-13 01:06:03 +0400

    add openlibrary api , update student ditail

--- library_db/app.py
+++ library_db/app.py
@@ -3,7 +3,7 @@ from sqlalchemy import select
 from db import engine
 from flask import Flask, render_template, redirect, request, url_for, session
 from library import Library, Students
-from models import BookModel, BookAuthorModel, UserModel, StudentModel, Book_CopiesModel
+from models import BookModel, BookAuthorModel, UserModel, StudentModel, Book_CopiesModel, BorrowsModel
 from forms import CreateBookForm, LoginForm, AdminRegisterForm, StudentRegisterForm
 from login import login_required
 import requests
@@ -51,13 +51,27 @@ def book_ditail(book_id):
     book_data = Library.select_get(book_id)
     book_copy_data = Library.select_get_book_copies(book_id)
     copies_count = Session(engine).query(Book_CopiesModel).where(Book_CopiesModel.book_id == book_id).count()
+    name = book_data.author.first_name + " " + book_data.author.last_name
+    print(name)
+    r1 = requests.get(f'https://openlibrary.org/search/authors.json?q={name}')
+    key = r1.json()["docs"][0]["key"]
+    author_bio = ""
+    r2 = requests.get(f'https://openlibrary.org/authors/{key}.json')
+    if r2.json().get("bio"):
+        if isinstance(r2.json().get("bio"), dict):
+            if r2.json()["bio"].get("value"):
+                author_bio = r2.json()["bio"].get("value")
+        else:
+            author_bio = r2.json().get("bio")
+
     for el in book_copy_data:
         r = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={[el.isbn]}'
-                                f'&key=AIzaSyDHSdWAbF5rMzG4LhNnDwmhvET1dtRXDYo')
+                         f'&key=AIzaSyDHSdWAbF5rMzG4LhNnDwmhvET1dtRXDYo')
         description_by_api = r.json()["items"][0]
         img_url = url_for('static', filename=f'/picture/id_{book_id}.jpeg')
+
         return render_template("book_ditail.html", book=book_data, img_url=img_url, book_copy_data=book_copy_data,
-                               description=description_by_api, copies_count=copies_count)
+                               description=description_by_api, copies_count=copies_count, author_bio=author_bio)
 
 
 @app.route("/books/add_new", methods=["GET", "POST"])
@@ -115,16 +129,16 @@ def list_students():
 @login_required
 def student_ditail(student_id):
     student = Session(engine).query(StudentModel).get(int(student_id))
+    borrow_ditail = Session(engine).scalars(
+        select(BorrowsModel).where(BorrowsModel.student_id == student_id)).fetchall()
     if request.method == "POST":
         if request.form.get("change"):
-            print("Change button is pressed !!!!!!")
-            print("===============================")
             return redirect(url_for(".update_form", student_id=student.id))
         elif request.form.get("del"):
             Students.delete_student_entity(student_id)
             return redirect("/students")
 
-    return render_template("student_ditail.html", student=student)
+    return render_template("student_ditail.html", student=student, borrow_ditail=borrow_ditail)
 
 
 @app.route('/update-form')
